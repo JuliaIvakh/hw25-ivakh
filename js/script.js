dd8e7994f474a6dfa1d3be6bec93af4aa45cd1b7
JuliaIvakh <85705848+JuliaIvakh@users.noreply.github.com>
2021-12-21 13:13:47 +0200

    hw25

---
+++ js/script.js
@@ -0,0 +1,44 @@
+(function () {
+  function counter() {
+    let arg = 0;
+    let counterStatistic = {
+      increase: 0,
+      decrease: 0,
+      get: 0,
+      getStatistic: 0,
+    };
+
+    const func = {
+      increase(num) {
+        counterStatistic.increase++;
+        return (arg += num);
+      },
+      decrease(num) {
+        counterStatistic.decrease++;
+        return (arg -= num);
+      },
+      get() {
+        counterStatistic.get++;
+        return arg;
+      },
+      getStatistic() {
+        counterStatistic.getStatistic++;
+        return counterStatistic;
+      },
+      zero() {
+        counterStatistic.increase = 0;
+        counterStatistic.decrease = 0;
+        counterStatistic.get = 0;
+        counterStatistic.getStatistic = 0;
+      },
+    };
+
+    return func;
+  }
+
+  const counterInst = counter();
+  counterInst.increase(105);
+  counterInst.decrease(50);
+  console.log(counterInst.get());
+  console.log(counterInst.getStatistic());
+})();
